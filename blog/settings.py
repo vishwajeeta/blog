7bf0de2ea0fdea5924dfe3d397bc45027de536ae
vishwajeeta <gs699542@gmail.com>
2024-10-20 19:14:39 +0530

    added copy button

--- blog/settings.py
+++ blog/settings.py
@@ -43,6 +43,16 @@ INSTALLED_APPS = [
     'ckeditor',
 ]
 
+CKEDITOR_CONFIGS={
+    'default':{
+        'toolbar':'full',
+        'extraPlugins':','.join(
+            [
+                'codesnippet'
+            ]
+        )
+    }
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -120,6 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR ,'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
